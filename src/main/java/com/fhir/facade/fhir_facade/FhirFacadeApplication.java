17521bcb8f65eb0d9cea303af9863e70cdee1f95
Shihang Wang <ucabsw2@ucl.ac.uk>
2018-08-01 11:10:02 +0100

    project init

---
+++ src/main/java/com/fhir/facade/fhir_facade/FhirFacadeApplication.java
@@ -0,0 +1,12 @@
+package com.fhir.facade.fhir_facade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhirFacadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FhirFacadeApplication.class, args);
+	}
+}
